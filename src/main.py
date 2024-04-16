3ee742f1a3ca856e31e82235034a814e0e7af4ff
azbozzetto <40679544+azbozzetto@users.noreply.github.com>
2024-04-16 14:02:41 -0300

    Update main.py test2

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def good_conditions():
+def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
